eef77b44b20dd16a6fcff912680de77048175def
jajalovack <dvtejuco@gmail.com>
2023-08-21 14:03:05 +0800

    test hero

---
+++ scripts/heroCarousel.js
@@ -0,0 +1,7 @@
+var animation=bodymovin.loadAnimation({
+    container: document.getElementById("carousel"),
+    renderer: "svg",
+    loop: true,
+    autoplay: true,
+    path: "./assets/heroSection/data.json"
+})
